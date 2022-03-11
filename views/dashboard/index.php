40a56659963860e804d12132402b18f5e64b364b
= <cuong06012000htk@gmail.com>
2022-03-11 10:16:12 +0700

    refs[TASk][6508]:hiện thị danh sách sản phẩm

--- views/dashboard/index.php
+++ views/dashboard/index.php
@@ -13,6 +13,7 @@
             background-color: #666;
             color: white;
         }
+
         .main-bar .content-nav .el1 a {
             color: white;
         }
@@ -24,12 +25,65 @@
         <?php require(__DIR__ . "/../layouts/sidebar.php") ?>
         <div class="content">
             <?php require(__DIR__ . "/../layouts/header-content.php") ?>
-            <div class="main-content">
-                <h1>Dask board</h1>
+            <table>
+                <tr class="tr1">
+                    <th id="th1">#</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Email Address</th>
+                </tr>
+                <tr>
+                    <td id="td1">1</td>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>markOtto@gmail.com</td>
+                </tr>
+                <tr>
+                    <td id="td1">2</td>
+                    <td>Jacob</td>
+                    <td>thrmton</td>
+                    <td>Jacobthorton@gmail.com</td>
+                </tr>
+                <tr>
+                    <td id="td1">3</td>
+                    <td>Larry</td>
+                    <td>the Bird</td>
+                    <td>larrybird@gmail.com</td>
+                </tr>
+                <tr>
+                    <td id="td1">4</td>
+                    <td>John</td>
+                    <td>Doe</td>
+                    <td>johndoe@gmail.com</td>
+                </tr>
+                <tr>
+                    <td id="td1">5</td>
+                    <td>Garry</td>
+                    <td>Bird</td>
+                    <td>Garrybrid@gmail.com</td>
+                </tr>
+            </table>
+            <div class="footer">
+                <div class="paginate">
+                    <div class="el el-start">
+                        <span>1</span>
+                    </div>
+                    <div class="el">
+                        <span>2</span>
+                    </div>
+                    <div class="el">
+                        <span>3</span>
+                    </div>
+                    <div class="el">
+                        <span>4</span>
+                    </div>
+                    <div class="el el-end">
+                        <span>5</span>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-    <script src="/aloBridge-function-login-demo/public/js/home.js"></script>
+        <script src="/aloBridge-function-login-demo/public/js/home.js"></script>
 </body>
 
 </html>
